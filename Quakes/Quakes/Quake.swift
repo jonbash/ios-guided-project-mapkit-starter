7577bbdea113be7628aaf079c3669b85410c778a
Jon Bash <jonbash@protonmail.com>
2020-01-16 10:23:18 -0800

    add quake model, mapkit framework

---
+++ Quakes/Quakes/Quake.swift
@@ -0,0 +1,54 @@
+//
+//  Quake.swift
+//  Quakes
+//
+//  Created by Jon Bash on 2020-01-16.
+//  Copyright © 2020 Lambda, Inc. All rights reserved.
+//
+
+import Foundation
+import MapKit
+
+// muse use subclass of NSObject to work with MapKit
+class Quake: NSObject, Decodable {
+    // magnitude
+    // place
+    // time
+    // coordinates
+    let magnitude: Double
+    let place: String
+    let time: Date
+    let latitude: Double
+    let longitude: Double
+
+    enum CodingKeys: String, CodingKey {
+        case magnitude = "mag"
+        case place
+        case time
+        case latitude
+        case longitude
+
+        // containers
+        case properties
+        case geometry
+        case coordinates
+    }
+
+    required init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        let properties = try container
+            .nestedContainer(keyedBy: CodingKeys.self, forKey: .properties)
+        var coordinates = try container
+            .nestedContainer(keyedBy: CodingKeys.self, forKey: .geometry)
+            .nestedUnkeyedContainer(forKey: .coordinates)
+
+        self.magnitude = try properties.decode(Double.self, forKey: .magnitude)
+        self.place = try properties.decode(String.self, forKey: .place)
+        self.time = Date(timeIntervalSince1970:
+            (try properties.decode(Double.self, forKey: .time)) / 1000)
+        self.longitude = try coordinates.decode(Double.self)
+        self.latitude = try coordinates.decode(Double.self)
+
+        super.init()
+    }
+}
